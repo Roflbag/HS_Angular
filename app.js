ef111796f2abd131bfa6359e61380a7c17f4407d
Roflbag <shpow678@gmail.com>
2015-02-13 20:05:31 -0800

    try

--- app.js
+++ app.js
@@ -1,48 +1,24 @@
 	
 
 
-	(function() {
   var app = angular.module('HearthStone_wiki', ['ngRoute']);
 
-  app.controller('StoreController', function() {
-    this.products = gems;
-  });
+  app.service("hsService", function($http, $q)
+  {
+    var deferred = $q.defer();
+    $http.get('hs_data.json').then(function(data){
+      deffered.resolve(data);
+    });
 
-  var gems = [{
-    name: 'Azurite',
-    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-    shine: 8,
-    price: 110.50,
-    rarity: 7,
-    color: '#CCC',
-    faces: 14,
-    images: [ ]
-  }, {
-    name: 'Bloodstone',
-    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-    shine: 9,
-    price: 22.90,
-    rarity: 6,
-    color: '#EEE',
-    faces: 12,
-    images: [
-      "images/gem-01.gif",
-      "images/gem-03.gif",
-      "images/gem-04.gif"
-    ]
-  }, {
-    name: 'Zircon',
-    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-    shine: 70,
-    price: 1100,
-    rarity: 2,
-    color: '#000',
-    faces: 6,
-    images: [
-      "images/gem-06.gif",
-      "images/gem-07.gif",
-      "images/gem-09.gif"
-    ]
-  }];
-})();
+    this.getPlayers = function(){
+      return deffered.promise;
+    }
+  })
+
+  .controller("hsController", function($scope, hsService){
+    var promist=hsService.getPlayers();
+    promose.then(function(data){
+      $scope.team = data;
+      console.log($scope.team);
+    })})
 
