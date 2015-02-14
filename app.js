65c828cc968ee08814fcba05c0a34047a44f369a
Roflbag <shpow678@gmail.com>
2015-02-14 12:52:39 -0800

    test json file

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 
 var app = angular.module('HearthStone_wiki', ['ngRoute']);
 
-function hsController($scope, $http) {
-    $http.get("hs_data.json")
+function hsController($scope,$http) {
+    $http.get("test.json")
     .success(function(response) {$scope.names = response;});
-  }
-
+}
