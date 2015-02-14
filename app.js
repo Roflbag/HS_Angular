5578e7283a5448a6f439e73b780f8f02cc65bf13
Roflbag <shpow678@gmail.com>
2015-02-13 20:21:14 -0800

    trying json

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
 	
 
 
-  var app = angular.module('HearthStone_wiki', ['ngRoute']);
+var app = angular.module('HearthStone_wiki', ['ngRoute']);
 
-  app.service("hsService", function($http, $q)
-  {
-    var deferred = $q.defer();
-    $http.get('hs_data.json').then(function(data){
-      deffered.resolve(data);
-    });
-
-    this.getPlayers = function(){
-      return deffered.promise;
-    }
-  })
-
-  .controller("hsController", function($scope, hsService){
-    var promist=hsService.getPlayers();
-    promose.then(function(data){
-      $scope.team = data;
-      console.log($scope.team);
-    })})
+function hsController($scope, $http) {
+    $http.get("hs_data.json")
+    .success(function(response) {$scope.names = response;});
+  }
 
