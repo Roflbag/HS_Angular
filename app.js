5c642d762234d2c107f6edf626300c559514bc37
Roflbag <shpow678@gmail.com>
2015-02-14 20:04:59 -0800

    got json data in

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-	
+	var app = angular.module('HearthStone_wiki', ['ngRoute']);
+
+	function hsController($scope, $http) {
+	    $http.get("hs_data.json")
+	        .success(function(response) {
+	            $scope.basic = response["Basic"];
+	        });
+
+	      $scope.showCard=function(card){
+	      	if(card.collectible===true){
+	      		return true;
+	      	}
+	      	return false;
+	      }
 
 
-var app = angular.module('HearthStone_wiki', ['ngRoute']);
 
-function hsController($scope,$http) {
-    $http.get("test.json")
-    .success(function(response) {$scope.names = response;});
-}
+	}
