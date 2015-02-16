5dbd897be6b8d6e764c81a6a19c33c33ce1a94bc
Roflbag <shpow678@gmail.com>
2015-02-16 12:52:40 -0800

    fix ooze bug

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-
-
-
-	var app = angular.module('HearthStone_wiki', ['ngRoute','ui.bootstrap']);
+var app = angular.module('HearthStone_wiki', ['ngRoute', 'ui.bootstrap']);
 
 
 
 
 
-	function hsController($scope, $http) {
-	    $http.get("hs_data.json")
-	        .success(function(response) {
-	            $scope.basic = response["Basic"];
-	        });
+function hsController($scope, $http) {
+    $http.get("hs_data.json")
+        .success(function(response) {
+            $scope.basic = response["Basic"];
+        });
 
-	    $scope.showCard = function(card) {
-	        if (card.collectible === true) {
-	            return true;
-	        }
-	        return false;
-	    }
+    $scope.showCard = function(card) {
+        if (card.collectible === true) {
+            return true;
+        }
+        return false;
+    }
 
-	    $scope.filterBeast = function (){
-	    	
-
-	    	return "Beast";
-	    };
-
-	    function resetPage(){
-	    	$route.reload();
-	    }
+     $scope.showZero = function(card) {
+        if (card.cost >= 0 || card.attack >= 0) {
+            return true;
+        }
+        return false;
+    }
 
 
-	}
+    $scope.reloadRoute = function() {
+        $route.reload();
+    }
+
+
+}
